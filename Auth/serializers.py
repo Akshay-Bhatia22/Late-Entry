98a8c096c57e540bf7d06cd9b9c44dc4a54be31f
Akshay-Bhatia22 <dpsvn.aksh06001@gmail.com>
2022-04-30 23:55:02 +0530

    File structure changes

--- Auth/serializers.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import field
-from rest_framework import serializers
-from Core.models import *
-
-class LateEntrySerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = LateEntry
-        fields = ['__all__']
